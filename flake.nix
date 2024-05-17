90f1a9cefe6540caf99b25047bbe21674f453535
Nafiz Islam <nafiz.islam1001@gmail.com>
2024-05-17 11:05:52 -0400

    add nix files

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+        {
+          devShell = with pkgs; mkShell {
+            buildInputs = [
+	            pkgs.bashInteractive
+              nodejs_20 nodePackages.npm nodePackages.typescript-language-server nodePackages.vscode-langservers-extracted nodePackages.eslint # node
+              sqlite
+            ];
+          };
+        });
+}
