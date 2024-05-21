3d048188db078a8b7c119fa0da184e76de4f3b89
Nafiz Islam <nafiz.islam1001@gmail.com>
2024-05-21 10:00:01 -0400

    correct sendEmail

--- triton-server/src/download/email.ts
+++ triton-server/src/download/email.ts
@@ -7,29 +7,34 @@ import { logger } from '../logger'
 
 
 export async function sendEmail(from: string, to: string, subject: string, content: string) {
-	return await new Promise<void>((resolve, reject) => {
-		const mailx = spawn('mailx', ['-s', subject], {
-			stdio: ['pipe', 'ignore', 'pipe']
-		})
-		mailx.stderr.on('data', (data) => {
-			// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-			logger.warn(`[mailx] ${data.toString()}`)
-		})
-		mailx.on('exit', () => resolve())
-		mailx.on('disconnect', resolve)
-		mailx.on('close', () => resolve())
-		mailx.on('error', (err) => {
-			logger.error(err, '[mailx]')
-			reject(err)
-		})
-		mailx.stdin.write(`${content}\n`, (err) => {
-			if (err) {
-				logger.error(err, '[mail]')
-				reject(err)
-			} else {
-				logger.debug(`[mailx] Finished writing to ${to}`)
-			}
-			mailx.kill()
-		})
+	const mailx = spawn('mailx', ['-s', subject], {
+		stdio: ['pipe', 'ignore', 'pipe']
 	})
+	try {
+		await new Promise<void>((resolve, reject) => {
+			mailx.stderr.on('data', (data) => {
+				logger.warn(`[mailx] ${data.toString()}`)
+			})
+			mailx.on('exit', () => resolve())
+			mailx.on('disconnect', () => resolve())
+			mailx.on('close', () => resolve())
+			mailx.on('error', (err) => {
+				logger.error(err, '[mailx]')
+				reject(err)
+			})
+			mailx.stdin.write(`${content}\n`, (err) => {
+				if (err) {
+					logger.error(err, '[mail]')
+					reject(err)
+				} else {
+					logger.debug(`[mailx] Finished writing to ${to}`)
+					resolve()
+				}
+			})
+		})
+	} catch (err) {
+		throw err
+	} finally {
+		mailx.kill()
+	}
 }
