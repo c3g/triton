3f8b67ecdecc0e5f6c93928ee6948d4d0a3a3aed
Nafiz Islam <nafiz.islam@computationalgenomics.ca>
2024-06-10 11:38:22 -0400

    use http

--- triton-server/src/magic/magic_axios.ts
+++ triton-server/src/magic/magic_axios.ts
@@ -37,7 +37,7 @@ const httpsProxy = new URL(clientPortalConfig.httpsProxy)
 const axiosProxyConfig: AxiosProxyConfig = {
 	host: httpsProxy.hostname,
 	port: Number(httpsProxy.port),
-	protocol: 'https'
+	protocol: 'http'
 }
 
 async function getToken(): Promise<string> {
