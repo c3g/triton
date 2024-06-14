a7c7d47c28fc4ca3e4e13a626317765daef0e190
Nafiz Islam <nafiz.islam1001@gmail.com>
2024-06-14 11:39:21 -0400

    bring back changes made

--- triton-server/src/download/schema.sql
+++ triton-server/src/download/schema.sql
@@ -1,9 +1,11 @@
 CREATE TABLE constants (
+  id INTEGER PRIMARY KEY ASC check(id = 1),
   http_project_size integer not null,
   globus_project_size integer not null,
   sftp_project_size integer not null
 );
 CREATE TABLE contacts (
+    id INTEGER PRIMARY KEY ASC,
     project_id text not null,
     depth      text,
     status     text check(status IN ('NEW', 'MODIFIED')) not null,
@@ -12,7 +14,7 @@ CREATE TABLE contacts (
    unique(project_id, type)
 );
 CREATE TABLE files (
-
+   id INTEGER PRIMARY KEY ASC,
    dataset_id text not null,
    source text not null,
    destination text not null,
@@ -41,19 +43,21 @@ CREATE TABLE historical_requests (
 
   , should_delete tinyint not null default 0);
 CREATE TABLE requests (
+   id INTEGER PRIMARY KEY ASC,
    status  TEXT CHECK( status IN ('REQUESTED', 'PENDING', 'SUCCESS', 'FAILED', 'QUEUED') ) NOT NULL DEFAULT 'REQUESTED',
 type TEXT CHECK( type IN ('HTTP', 'SFTP', 'GLOBUS') ) NOT NULL,
    dataset_id text not null,  
 
    completion_date text,
+   failure_date text,
 
    expiry_date text,
    creation_date text,
 
-   project_id text not null, should_delete tinyint not null default 0, notified        tinyint not null default 0, requester       text,
-
-   UNIQUE(TYPE,  dataset_id)
-
+   project_id text not null,
+   should_delete tinyint not null default 0,
+   notification_date text,
+   requester       text,
   );
 CREATE INDEX idx_dataset_files on files(dataset_id);
-CREATE INDEX idx_dataset_request on requests(dataset_id);
+CREATE UNIQUE INDEX idx_dataset_request on requests(dataset_id);
