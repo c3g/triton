0391ccbb28d5ce9e77056acb74f7068873793bb7
Nafiz Islam <nafiz.islam@computationalgenomics.ca>
2024-06-20 11:38:30 -0400

    move prettier and eslint into root

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,9 +6,15 @@ import pluginReactConfig from "eslint-plugin-react/configs/recommended.js"
 export default [
     { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
     { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-    { languageOptions: { globals: globals.browser } },
+    { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
     pluginReactConfig,
-    { ignores: [".storybook", "build", "public", "storybook-static"] },
+    { ignores: ["triton-client/build/", "triton-client/public/", "triton-server/data/", "triton-server/scripts/"] },
+    {
+        rules: {
+            "react/react-in-jsx-scope": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+        },
+    },
 ]
--- triton-server/jest.config.ts
+++
@@ -1,7 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-export const preset = 'ts-jest'
-export const testEnvironment = 'node'
-export default {
-	preset,
-	testEnvironment,
-}
