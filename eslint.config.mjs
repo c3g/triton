e87f26e5f169f56f209c790785aa752282a8fea8
Nafiz Islam <nafiz.islam1001@gmail.com>
2024-06-20 14:40:27 -0400

    Refactor some configs (#37)
    * update pretty config up to ulysse's taste
    
    * simplify triton-server
    
    * refactor eslint
    
    * more refactoring
    
    * more refactoring
    
    * refactor config for magic-mock
    
    * add editorconfig
    
    * update .prettierrc
    
    * more tweaks
    
    * handle jsons gracefully
    
    * syke
    
    * json is 4 spaces
    
    * downgrade eslint
    
    * eslint ignore some files
    
    * move prettier and eslint into root
    
    * pretty ignore .vscode
    
    * Revert "pretty ignore .vscode"
    
    This reverts commit 68acb1bdf3d3ed14b86b489a2ac86a0a5a9e36db.
    
    * specify react version
    
    * ignore public except html, js, json
    
    * remove max line length
    
    * remove globals and nodemon from triton-types

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import globals from "globals"
+import pluginJs from "@eslint/js"
+import tseslint from "typescript-eslint"
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js"
+
+export default [
+    { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+    { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+    { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    pluginReactConfig,
+    { settings: { react: { version: "18.0.15" } } },
+    { ignores: ["triton-client/build/", "triton-client/public/", "triton-server/data/", "triton-server/scripts/"] },
+    {
+        rules: {
+            "react/react-in-jsx-scope": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+        },
+    },
+]
