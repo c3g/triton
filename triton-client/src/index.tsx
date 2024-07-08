c7ce5c91e3dac5e3c7fac7bb1ba54327747033af
sebastian <sebastian.amouzegar@computationalgenomics.ca>
2024-07-08 16:35:17 -0400

    more changes

--- triton-client/src/index.tsx
+++ triton-client/src/index.tsx
@@ -9,7 +9,6 @@ import "./index.css"
 // Log the environment that was used to build the client so that we can verify
 // that the right build is running in dev/prod.
 console.log(`ENVIRONMENT: ${process.env.NODE_ENV}`)
-
 const container = document.getElementById("root")!
 const root = createRoot(container)
 
