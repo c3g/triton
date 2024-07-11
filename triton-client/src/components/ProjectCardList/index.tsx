0bf9488c34f2c401d3a12a85a0236d917004e31c
Nafiz Islam <nafiz.islam1001@gmail.com>
2024-07-11 13:25:36 -0400

    fix missing interface rename

--- triton-client/src/components/ProjectCardList/index.tsx
+++ triton-client/src/components/ProjectCardList/index.tsx
@@ -1,15 +1,15 @@
 import "@components/Common.scss"
 import { useNavigate } from "react-router-dom"
 import { Button, Typography } from "antd"
-import { IProjectCard, IProjectCardListProps } from "./interfaces"
+import { ProjectCard, ProjectCardListProps } from "./interfaces"
 
-export default function ProjectCardList({ projects }: IProjectCardListProps) {
+export default function ProjectCardList({ projects }: ProjectCardListProps) {
     const navigate = useNavigate()
     const { Title } = Typography
     return (
         <>
             <Title level={2}>Projects</Title>
-            {projects.map((project: IProjectCard) => (
+            {projects.map((project: ProjectCard) => (
                 <Button
                     key={project.external_id}
                     block
