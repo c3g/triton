86ed40fe9c161bbf8728bc11c17175d6c1ebd15a
Nafiz Islam <nafiz.islam1001@gmail.com>
2024-07-23 16:04:04 -0400

    desperate attempt at scrolling

--- triton-client/src/components/ReadsPerSample/index.tsx
+++ triton-client/src/components/ReadsPerSample/index.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, useCallback, useEffect, useMemo } from "react"
+import { ReactElement, useCallback, useEffect, useMemo, useState } from "react"
 import ReadsPerSampleProps, { ReadsPerSampleButtonProps } from "./interfaces"
 import { useAppSelector } from "@store/hooks"
 import { selectReadsPerSample } from "@store/selectors"
@@ -43,22 +43,61 @@ interface CorrectedBarConfig extends Omit<BarConfig, "sort"> {
 function ReadsPerSampleGraph(
     readsPerSample: NonNullable<DatasetState["readsPerSample"]>,
 ) {
-    const config: CorrectedBarConfig = useMemo(() => {
-        return {
-            data: readsPerSample.sampleReads.map((numberOfReads) => ({
+    const data = useMemo(
+        () =>
+            readsPerSample.sampleReads.map((numberOfReads) => ({
                 sample: numberOfReads.sampleName,
                 reads: numberOfReads.nbReads,
             })),
+        [readsPerSample],
+    )
+
+    const [scrollPercentage, setScrollPercentage] = useState(0)
+
+    const config: CorrectedBarConfig = useMemo(() => {
+        return {
+            data,
             sort: {
                 by: "y",
                 reverse: true,
             },
             xField: "sample",
             yField: "reads",
+            // https://ant-design-charts.antgroup.com/en/options/plots/component/scrollbar
+            scrollbar: {
+                x: {
+                    ratio: 0.005,
+                    slidable: false,
+                    scrollable: false,
+                    value: scrollPercentage,
+                },
+            },
         }
-    }, [readsPerSample])
+    }, [data, scrollPercentage])
 
-    return <Bar {...config} />
+    return (
+        <>
+            <Button
+                onClick={() => {
+                    setScrollPercentage(
+                        Math.max(scrollPercentage - 1 / data.length, 0.0),
+                    )
+                }}
+            >
+                Scroll Up
+            </Button>
+            <Button
+                onClick={() => {
+                    setScrollPercentage(
+                        Math.min(scrollPercentage + 1 / data.length, 1.0),
+                    )
+                }}
+            >
+                Scroll Down
+            </Button>
+            <Bar {...config} />
+        </>
+    )
 }
 
 export function ReadsPerSampleButton({
