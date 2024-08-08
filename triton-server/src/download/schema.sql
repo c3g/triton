5d36b06464ba51a299276d436c2c1878f664fe56
Nafiz Islam <nafiz.islam@computationalgenomics.ca>
2024-08-08 12:16:29 -0400

    wip

--- triton-server/src/download/schema.sql
+++ triton-server/src/download/schema.sql
@@ -79,8 +79,8 @@ create table constants (
     expiry_days         INTEGER not null default 7
 );
 
-create table notification_dates {
+create table notification_dates (
     id                              INTEGER PRIMARY KEY ASC check(id = 1),
-    last_released_notification_date text not null, -- ISO8601
-}
+    last_released_notification_date text not null -- ISO8601
+);
 insert into notification_dates (last_released_notification_date) values (datetime('now')); -- avoiding notification for old datasets
