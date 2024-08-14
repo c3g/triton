f66447cfb08aad2af1170a667a4a4a8215b466e7
sebastian <sebastian.amouzegar@computationalgenomics.ca>
2024-08-14 16:08:09 -0400

    final copy changes as per other team members

--- triton-client/src/components/Landing/index.tsx
+++ triton-client/src/components/Landing/index.tsx
@@ -54,40 +54,41 @@ function LandingPage({ isLoggedIn, userName }: UserInformationProps) {
                 <Text className="Common-subtitle Landing-user" type="secondary">
                     {isLoggedIn ? `Logged in as ${userName}` : "Logging In.."}
                 </Text>
-                <Typography.Title level={2}>How to stage</Typography.Title>
+                <Typography.Title level={2}>
+                    How to stage Datasets
+                </Typography.Title>
                 <Content className="faq-container">
                     <div className="faq-section-container">
                         <Typography.Title level={5}>
-                            1. You will be able to stage and/or unstage the
-                            datasets on either SFTP or GLOBUS. We strongly
-                            suggest to use GLOBUS as it is faster than SFTP.
+                            1. Datasets can be staged and/or unstaged using
+                            either SFTP or GLOBUS. GLOBUS is strongly
+                            recommended, as it is faster than SFTP.
                         </Typography.Title>
                         <Typography.Title level={5}>
-                            2. You will be able to stage with either one or the
-                            other of the 2 methods available (via SFTP or
-                            GLOBUS) for each dataset. Once the datasets are
-                            staged, you will a fixed amount of time (7 days) to
-                            download the files before they get automatically
-                            unstaged from the server. It is necessary that you
-                            unstage manually (click on the same button again)
-                            the files once you have finished the download in
-                            order to continue staging and then download other
-                            files.
+                            2. For each dataset, staging can be done using one
+                            of the two available methods: SFTP or GLOBUS. Once
+                            the datasets have been staged, a fixed period of 7
+                            days is provided to download the files before they
+                            are automatically unstaged from the server. After
+                            the download is completed, the files must be
+                            manually unstaged to allow further staging and
+                            downloading of other files.
                         </Typography.Title>
                         <Typography.Title level={5}>
-                            3. There are 4 request status when staging the
-                            datasets. Unstaging, Stage, Queued and Failed. If
-                            you see Queued, it means that the dataset is waiting
-                            to have enough space in the disc to start staging.
-                            If you see Failed, you can communicate with{" "}
-                            <Link to={""}>{"it.genome@mail.mcgill.ca"}</Link>
+                            3. There are five request statuses available when
+                            datasets are being staged: "Unstaging", "Staging",
+                            "Staged" (shown as "Download"), "Queued", and
+                            "Failed". If "Queued" is displayed, the dataset is
+                            waiting for sufficient disk space to begin staging.
+                            If "Failed" is displayed, please contact us at{" "}
+                            <Link to={""}>{"hercules@mcgill.ca"}</Link>
                         </Typography.Title>
                         <Typography.Title level={5}>
-                            4. There is an info icon {"  "}
-                            <Button icon={<InfoCircleOutlined />} /> {"  "}
-                            associated to each dataset released. The graph can
-                            help you visualize, in more details, the amount of
-                            readsets/datasets released.
+                            4. An info icon {"  "}
+                            <Button icon={<InfoCircleOutlined />} /> {"  "} is
+                            associated with each dataset released. Clicking it
+                            will show a graph with reads per sample of that
+                            dataset, and the number of readsets. (edited)
                         </Typography.Title>
                     </div>
                 </Content>
@@ -97,3 +98,8 @@ function LandingPage({ isLoggedIn, userName }: UserInformationProps) {
 }
 
 export default LandingPage
+
+1
+2
+3
+4
