ef3cc73f5b7148344ecc8a5da2cd9f620a294390
Nafiz Islam <nafiz.islam1001@gmail.com>
2024-10-08 13:45:14 -0400

    MCG to MGC (#103)

--- triton-server/src/notification.ts
+++ triton-server/src/notification.ts
@@ -146,7 +146,7 @@ export const sendLatestReleasedNotificationEmail = async () => {
                                 -   <b>Dataset ID: ${dataset.id}</b><br/>
                                 -   Dataset Lane: ${dataset.lane}<br/>
                                 -   Dataset release time: ${new Date(dataset.latest_release_update).toUTCString()} (UTC)<br/><br/>
-                                Datasets can be downloaded from the MCG Data Portal.
+                                Datasets can be downloaded from the MGC Data Portal.
                                 To access the Data Portal, please login to your Hercules account and click on the Data Portal button on the top menu.<br/>
                                 Datasets can be downloaded using SFTP or Globus using the credential provided to you during the staging process.<br/><br/>
                                 If you forgot or didn't receive your credential, you can reset your password in the Data Portal.<br/>
